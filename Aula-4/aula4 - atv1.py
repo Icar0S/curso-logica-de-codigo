040c4361faa848b65bca2dac09c349f209a52cf9
Icar0S <icarooliveira_23@hotmail.com>
2025-01-31 23:01:25 -0300

    add atv1

---
+++ Aula-4/aula4 - atv1.py
@@ -0,0 +1,4 @@
+# Crie um programa que use um laço for para imprimir os números de 1 a 10 no console.
+
+for numero in range(1, 11):  # O último número não é incluso, então vai de 1 a 10
+    print(f"Número atual: {numero}")
