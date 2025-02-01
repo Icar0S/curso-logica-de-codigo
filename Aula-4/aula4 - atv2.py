dcca2ad9cb57b967a40d3a3c2c303de894b96903
Icar0S <icarooliveira_23@hotmail.com>
2025-01-31 23:02:16 -0300

    add atv2

---
+++ Aula-4/aula4 - atv2.py
@@ -0,0 +1,6 @@
+# Escreva um programa que declare uma lista com os números [1120, 1220, 3230, 4043, 5430] e exiba o primeiro elemento.
+
+
+numeros = [1120, 1220, 3230, 4043, 5430]
+print(f"Lista completa: {numeros}")
+print(f"Primeiro elemento: {numeros[0]}")
