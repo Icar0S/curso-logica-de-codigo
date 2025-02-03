98a86a8953d3e7ef2b186858a6391d3b0c1a0993
Icar0S <icarooliveira_23@hotmail.com>
2025-02-03 13:54:01 -0300

    add formate

--- Aula-4/aula4 - forAlto.py
+++ Aula-4/aula4 - forAlto.py
@@ -1,5 +1,11 @@
 # Lista de tuplas com o nome e a altura dos alunos
-alunos = [("João", 2.75), ("Maria", 1.68), ("Pedro", 1.82), ("Ana", 1.70), ("Carlos", 1.90)]
+alunos = [
+    ("João", 2.75),
+    ("Maria", 1.68),
+    ("Pedro", 1.82),
+    ("Ana", 1.70),
+    ("Carlos", 1.90),
+]
 
 # Inicializando variáveis para armazenar o aluno mais alto
 aluno_mais_alto = alunos[0]
@@ -12,7 +18,7 @@ for i in range(1, len(alunos)):
         indice_mais_alto = i
 
 # Removendo o aluno mais alto da lista
-#alunos.pop(indice_mais_alto)
+# alunos.pop(indice_mais_alto)
 
 # Exibindo o nome do aluno mais alto removido e a lista atualizada
 print(f"Aluno mais alto é: {aluno_mais_alto[0]} com {aluno_mais_alto[1]}m de altura.")
